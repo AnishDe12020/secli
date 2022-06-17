98a7cffec589b053c6223975cdba866d86ca96a0
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-06-17 15:05:54 +0530

    chore(list): add colors, add a message if no secret was found

--- src/core/commands/list.rs
+++ src/core/commands/list.rs
@@ -1,6 +1,7 @@
 use crate::{cli::app::App, core::db::get_secrets};
 use anyhow::Result;
 use clap::Values;
+use colored::Colorize;
 use rusqlite::Connection;
 
 pub fn list(app: App, _args: Option<Values<'_>>) -> Result<()> {
@@ -8,14 +9,30 @@ pub fn list(app: App, _args: Option<Values<'_>>) -> Result<()> {
 
     let secrets = get_secrets(&conn).unwrap();
 
+    let num_secrets = secrets.len();
+
+    if num_secrets == 0 {
+        println!(
+            "No secrets found. Add one by running {}{}{}",
+            "`".cyan(),
+            "secli add".green(),
+            "`".cyan()
+        );
+        return Ok(());
+    }
+
     println!(
-        "{} {} found\n",
-        secrets.len(),
-        if secrets.len() == 1 {
-            "secret"
-        } else {
-            "secrets"
-        }
+        "{}",
+        format!(
+            "{} {} found",
+            secrets.len(),
+            if secrets.len() == 1 {
+                "secret"
+            } else {
+                "secrets"
+            }
+        )
+        .green()
     );
 
     for secret in secrets {
