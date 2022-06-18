f4cfe7b2af955e6848eba167006c436a1232e269
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-06-18 13:50:34 +0530

    chore(update): check if secret exists before deleting

--- src/core/commands/update.rs
+++ src/core/commands/update.rs
@@ -1,5 +1,5 @@
 use crate::cli::app::App;
-use crate::core::db::{update_secret, Secret};
+use crate::core::db::{get_secret, update_secret, Secret};
 use anyhow::Result;
 use clap::Values;
 use colored::Colorize;
@@ -19,22 +19,30 @@ pub fn update(app: App, _args: Option<Values>) -> Result<()> {
 
     let secret = Secret { name, value };
 
-    match update_secret(&conn, &secret) {
-        Ok(_) => {
+    match get_secret(&conn, &secret.name) {
+        Ok(_) => match update_secret(&conn, &secret) {
+            Ok(_) => {
+                println!(
+                    "{}",
+                    format!(
+                        "Secret with name {}{}{} updated successfully",
+                        "`".yellow(),
+                        secret.name.cyan(),
+                        "`".yellow()
+                    )
+                    .green(),
+                );
+            }
+            Err(err) => {
+                println!("{}", err.to_string().red());
+                return Ok(());
+            }
+        },
+        Err(_) => {
             println!(
                 "{}",
-                format!(
-                    "Secret with name {}{}{} updated successfully",
-                    "`".yellow(),
-                    secret.name.cyan(),
-                    "`".yellow()
-                )
-                .green(),
+                format!("Secret with name {} doesn't exist", &secret.name.cyan()).red()
             );
-        }
-        Err(err) => {
-            println!("{}", err.to_string().red());
-            return Ok(());
         }
     }
 
